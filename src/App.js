e891e1972847afd54751ea353bfb9c123436d895
hendrik-boerma <hendrikboerma@gmail.com>
2023-02-01 12:30:01 +0100

    skills

--- src/App.js
+++ src/App.js
@@ -69,6 +69,10 @@ function App() {
           <button  onClick={() => collapsible("p5")}>Studie Communication & Multimedia design - Hanze Hogeschool</button>
           <p id="p5">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         </section>
+        <section className='container2'>
+          <h3>Skills</h3>
+          <p></p>
+        </section>
         <section className='container3'>
           <h3 id='portfolio'>Portfolio</h3>
           <div className='gallery'>
