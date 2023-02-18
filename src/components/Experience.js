c83e9c1ae89f47779dfb91a86842e0ece45496e0
hendrik-boerma <hendrikboerma@gmail.com>
2023-02-18 23:31:24 +0100

    wip

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,39 +2,30 @@ import images from '../images'
 
 function Experience () {
 
-    function collapsible(num) {
-        const paragraf = document.getElementById(num)
-        if (paragraf.style.display === "none") {
-          paragraf.style.display = "block";
-        } else {
-          paragraf.style.display = "none"
-        }
-      }
-
     return (
         <section className='container2'>
-          <div className='ervaring'>
+          <div className='ervaring py-12'>
             <h3 id='ervaring'>Ervaring</h3>
-            <button onClick={() => collapsible("p1")}>Agile Test Engineer - Sogeti</button>
-            <p id="p1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-            <button onClick={() => collapsible("p2")}>E-Commerce medewerker - A4H-TECH</button>
-            <p id="p2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-            <button  onClick={() => collapsible("p3")}>UX Designer - DEAN</button>
-            <p id="p3">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-            <button  onClick={() => collapsible("p4")}>Grafisch Vormgever - Adventure Works SA</button>
-            <p id="p4">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-            <button  onClick={() => collapsible("p5")}>Studie Communication & Multimedia design - Hanze Hogeschool</button>
-            <p id="p5">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <button>Agile Test Engineer - Sogeti</button>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <button>E-Commerce medewerker - A4H-TECH</button>
+            <p >Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <button>UX Designer - DEAN</button>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <button>Grafisch Vormgever - Adventure Works SA</button>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <button>Studie Communication & Multimedia design - Hanze Hogeschool</button>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
           </div>
-          <div className='skills'>
+          <div className='md:w-1/2 py-12'>
             <h3>Skills</h3>
-            <div className='skillsicons'>
-              <img src={images.htmlicon} alt='Hendrik Boerma' />
-              <img src={images.cssicon} alt='Hendrik Boerma' />
-              <img src={images.jsicon} alt='Hendrik Boerma' />
-              <img src={images.figmaicon} alt='Hendrik Boerma' />
-              <img src={images.adobeicon} alt='Hendrik Boerma' />
-              <img src={images.tmapicon} alt='Hendrik Boerma' />
+            <div className='flex flex-wrap justify-center items-center md:justify-start'>
+              <img className='p-6 w-40 h-full ' src={images.htmlicon} alt='Hendrik Boerma' />
+              <img className='p-6 w-40 h-full'  src={images.cssicon} alt='Hendrik Boerma' />
+              <img className='p-6 w-40 h-full'  src={images.jsicon} alt='Hendrik Boerma' />
+              <img className='p-6 w-40 h-full'  src={images.figmaicon} alt='Hendrik Boerma' />
+              <img className='p-6 w-40 h-full'  src={images.adobeicon} alt='Hendrik Boerma' />
+              <img className='p-6 w-40 h-full'  src={images.tmapicon} alt='Hendrik Boerma' />
             </div>
           </div>
         </section>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,19 @@
+import linkedin from '../images/icons/linkedin.png'
+import mail from '../images/icons/mail.png'
+
 function Footer () {
+
+
     return (
-    <footer>
-        <h3>Contact</h3>
-    </footer>
+        <footer className='p-16 bg-black h-96'>
+        <h3 className='text-3xl md:text-4xl font-futura flex justify-center'>Contact</h3>
+            <div className='flex flex-row items-center justify-center gap-24 md:flex-wrap'>
+                <div className='flex gap-12 py-16 cursor-pointer md:flex-wrap'>
+                    <i className="w-20 cursor-pointer"><a href='https://www.linkedin.com/in/hendrik-boerma' ><img src={linkedin} alt='linkedinicon'/></a></i>
+                    <i className="w-20 cursor-pointer"><a href='mailto:hendrikboerma@gmail.com' ><img src={mail} alt='mailicon'/></a></i>
+                </div>
+            </div>
+        </footer>
     );
 }
 
