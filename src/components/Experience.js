7ad23d987ad2ba795771d65f56d97c87cf10ed0c
hendrik-boerma <hendrikboerma@gmail.com>
2023-03-03 23:51:34 +0100

    WI

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -6,58 +6,50 @@ const arrow = <FontAwesomeIcon icon={faSortDown} size="xs" />
 
 function Experience () {
 
-  function collapsible (contentid, arrowid){
-    const content = document.getElementById(contentid)
-    const arrow = document.getElementById(arrowid)
-    
-    if (content.style.display === "block") {
-      content.style.display = "none"
-      arrow.style.rotate = "360deg"
-    } else {
-      content.style.display = "block"
-      arrow.style.rotate = "180deg"
+  const Button = ({title, company, arrowid, contentid}) => {
+  
+    function collapsible (){
+      const content = document.getElementById(contentid)
+      const arrow = document.getElementById(arrowid)
+      
+      if (content.style.display === "block") {
+        content.style.display = "none"
+        arrow.style.rotate = "360deg"
+      } else {
+        content.style.display = "block"
+        arrow.style.rotate = "180deg"
+      }
     }
+    
+    return (
+      <button className=" flex justify-between items-center w-full p-4 pl-6 text-left cursor-pointer bg-mediumblue rounded font-bold" 
+      onClick={() => collapsible(contentid, arrowid)}>
+      <span>{title} <br/><span className='font-normal'>{company}</span></span>
+      <span id={arrowid} className='text-2xl text-center'>{arrow}</span>
+      </button>
+    );
   }
 
 
     return (
-        <section className='text-offwhite py-12 px-6 bg-darkblue md:px-16' id='section2'>
+      <section className='text-offwhite py-12 px-6 bg-darkblue md:px-16' id='section2'>
         <h3 className='text-4xl py-8 font-bold' id='ervaring'>Ervaring</h3>
         <div className='flex flex-col md:flex-row md:gap-16'>
           <div className='flex flex-col gap-4 md:w-6/12'>
             <h4 className='text-2xl text-offwhite' id='werk'>Werk</h4>
-              <button className=" flex justify-between items-center w-full p-4 pl-6 text-left cursor-pointer bg-mediumblue rounded font-bold" onClick={() => collapsible('content1', 'arrow1')}>
-                 <span>Agile Test Engineer <br/><span className='font-normal'>Sogeti Nederland</span></span>
-                 <span id='arrow1' className='text-2xl text-center'>{arrow}</span>
-              </button>
+            <Button title="Agile Test Engineer" company="Sogeti Nederland" arrowid="arrow1" contentid="content1" />
               <p className="hidden px-6 pb-6 text-justify leading-loose" id='content1'>Momenteel werk ik binnen Sogeti aan een project voor de gemeente Groningen. Samen met andere Sogetisten ontwikkel ik een applicatie om mensen uit Eritrea te helpen met het leren van de Nederlandse taal. In dit project ben ik samen met een collega verantwoordelijk voor het ontwerpen en bouwen van de front-end in ReactJS. Om de backlog bij te houden en samen te werken als developers maken we gebruik van Azure DevOps en Git.
-                Naast het project voor de gemeente Groningen ben ik bezig om mijn kennis verder uit te breiden. Ik heb onder andere mijn AZ-900 certificaat behaald en ben bezig om verschillende tools en frameworks beter onder de knie te krijgen. Momenteel ben ik bezig met een front-end opleiding van Meta op Coursera om de basis van front-end development te leren. Daarnaast verdiep ik mij in test automation tools zoals Cypress.
-              </p>
-              <button className=" flex justify-between items-center w-full p-4 pl-6 text-left cursor-pointer bg-mediumblue rounded font-bold" onClick={() => collapsible('content2', 'arrow2')}>
-                 <span>E-Commerce Medewerker<br/><span className='font-normal'>A4H-TECH BV</span></span>
-                 <span id='arrow2' className='text-2xl'>{arrow}</span>
-              </button>
+                Naast het project voor de gemeente Groningen ben ik bezig om mijn kennis verder uit te breiden. Ik heb onder andere mijn AZ-900 certificaat behaald en ben bezig om verschillende tools en frameworks beter onder de knie te krijgen. Momenteel ben ik bezig met een front-end opleiding van Meta op Coursera om de basis van front-end development te leren. Daarnaast verdiep ik mij in test automation tools zoals Cypress.</p>
+              <Button title="E-Commerce Medewerker" company="A4H-TECH BV" arrowid="arrow2" contentid="content2" />
               <p className="hidden px-6 pb-6 text-justify leading-loose" id='content2'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-              <button className=" flex justify-between items-center w-full p-4 pl-6 text-left cursor-pointer bg-mediumblue rounded font-bold" onClick={() => collapsible('content3', 'arrow3')}>
-                 <span>UX Designer<br/><span className='font-normal'>Digital Education Africa Network</span></span>
-                 <span id='arrow3' className='text-2xl'>{arrow}</span>
-              </button>
+              <Button title="UX Designer" company="Digital Education Africa Network" arrowid="arrow3" contentid="content3" />
               <p className="hidden px-6 pb-6 text-justify leading-loose" id='content3'> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-              <button className=" flex justify-between items-center w-full p-4 pl-6 text-left cursor-pointer bg-mediumblue rounded font-bold" onClick={() => collapsible('content4', 'arrow4')}>
-                 <span>Grafisch Vormgever<br/><span className='font-normal'>Adventure Works</span></span>
-                 <span id='arrow4' className='text-2xl'>{arrow}</span>
-              </button>
+              <Button title="Grafisch Vormgever" company="Adventure Works" arrowid="arrow4" contentid="content4" />
               <p className="hidden px-6 pb-6 text-justify leading-loose" id='content4'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
             <h4 className='text-2xl pt-4 text-offwhite' id='werk'>Studie</h4>
-            <button className=" flex justify-between items-center w-full p-4 pl-6 text-left cursor-pointer bg-mediumblue rounded font-bold" onClick={() => collapsible('content5', 'arrow5')}>
-                 <span>Front-End Developer<br/><span className='font-normal'>Meta - Coursera</span></span>
-                 <span id='arrow5' className='text-2xl'>{arrow}</span>
-              </button>
+            <Button title="Front-End Developer" company="Meta - Coursera" arrowid="arrow5" contentid="content5" />
               <p className="hidden px-6 pb-6 text-justify leading-loose" id='content5'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-              <button className=" flex justify-between items-center w-full p-4 pl-6 text-left cursor-pointer bg-mediumblue rounded font-bold" onClick={() => collapsible('content6', 'arrow6')}>
-                 <span>CMD - Visual Design<br/><span className='font-normal'>Hanzehogeschool Groningen</span></span>
-                 <span id='arrow6' className='text-2xl'>{arrow}</span>
-              </button>
+              <Button title="CMD - Visual Design" company="Hanzehogeschool Groningen" arrowid="arrow6" contentid="content6" />
               <p className="hidden px-6 pb-6 text-justify leading-loose" id='content6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
           </div>
           <div className='md:w-6/12'>
@@ -86,8 +78,8 @@ function Experience () {
               <span className='bg-mediumblue p-4 grow rounded'>Scrum</span>
             </div>
           </div>
-          </div>
-        </section>
+        </div>
+      </section>
     );
 }
 
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -13,17 +13,23 @@ function Portfolio() {
         modal.style.display = "none";
     }
 
+    const Imagebox = ({boximage, alttext}) => {
+                return (
+            <img className='rounded max-h-96 grow sm:w-4/12 lg:w-3/12 object-cover align-bottom cursor-pointer' onClick={() => modal(boximage)} src={boximage} alt={alttext} />
+        );
+    }
+
     return (
         <>
             <section className='py-12 px-6 md:px-16 bg-darkblue'  id='section3'>
                 <h3 className='text-4xl py-8 text-offwhite font-bold'>Portfolio</h3>
                 <div className='flex flex-wrap gap-2'>
-                    <img className='rounded max-h-96 grow sm:w-4/12 lg:w-3/12 object-cover align-bottom cursor-pointer' onClick={() => modal(images.a4hflyer)} src={images.a4hflyer} alt='cape town' />
-                    <img className='rounded max-h-96 grow sm:w-4/12 lg:w-3/12 object-cover align-bottom cursor-pointer'  onClick={() => modal(images.illustrations)} src={images.illustrations} alt='Hendrik Boerma' />
-                    <img className='rounded max-h-96 grow sm:w-4/12 lg:w-3/12 object-cover align-bottom cursor-pointer'  onClick={() => modal(images.landscape)} src={images.landscape} alt='Hendrik Boerma' />
-                    <img className='rounded max-h-96 grow sm:w-4/12 lg:w-3/12 object-cover align-bottom cursor-pointer'  onClick={() => modal(images.mockup)} src={images.mockup} alt='Hendrik Boerma' />
-                    <img className='rounded max-h-96 grow sm:w-4/12 lg:w-3/12 object-cover align-bottom cursor-pointer'  onClick={() => modal(images.voucheraw)} src={images.voucheraw} alt='Hendrik Boerma' />
-                    <img className='rounded max-h-96 grow sm:w-4/12 lg:w-3/12 object-cover align-bottom cursor-pointer'  onClick={() => modal(images.voucheraw)} src={images.voucheraw} alt='Hendrik Boerma' />
+                    <Imagebox boximage={images.a4hflyer} alttext="a4hflyer"/>
+                    <Imagebox boximage={images.illustrations} alttext="illustrations"/>
+                    <Imagebox boximage={images.landscape} alttext="landscape"/>
+                    <Imagebox boximage={images.mockup} alttext="mockup"/>
+                    <Imagebox boximage={images.voucheraw} alttext="voucheraw"/>
+                    <Imagebox boximage={images.voucheraw} alttext="voucheraw"/>
                 </div>
             </section>
             <div onClick={close} className=" flex flex-col hidden fixed items-center text-end justify-center w-full top-0 h-full z-30 bg-blacktransparent" id='modal'>    
