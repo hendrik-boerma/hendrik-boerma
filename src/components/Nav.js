f23213752d44a424471511adbd8590fccd3e3962
hendrik-boerma <hendrikboerma@gmail.com>
2023-06-30 22:47:40 +0200

    nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -41,22 +41,6 @@ function openMenu () {
     </div>
     </nav>
     )
-
-    return (
-        <nav className="fixed flex justify-between w-full text-lightgrey py-4 bg-darkblue drop-shadow-md z-20 top-0">
-          <ul className="flex justify-start md:px-16 md:gap-16 gap-8">
-            <a href="https://www.linkedin.com/in/hendrik-boerma/" ><FontAwesomeIcon className="cursor-pointer hover:text-neoblue" icon={faLinkedinIn} size='xl'/></a>
-            <a href="https://github.com/hendrik-boerma"><FontAwesomeIcon className="cursor-pointer hover:text-neoblue" icon={faGithub} size='xl'/></a>
-            <a href="mailto:hendrikboerma@gmail.com"><FontAwesomeIcon className="cursor-pointer hover:text-neoblue" icon={faEnvelope} size='xl'/></a>
-          </ul>
-          <ul className="flex justify-end md:justify-end md:px-16 md:gap-16 gap-8">
-            <li className="cursor-pointer"><a className="hover:border-b-2 border-neoblue py-4 decoration-2 underline-offset-8" href="#section1">Over mij</a></li>
-            <li className="cursor-pointer"><a className="hover:border-b-2 border-neoblue py-4 decoration-2 underline-offset-8" href="#section2">Ervaring</a></li>
-            <li className="cursor-pointer"><a className="hover:border-b-2 border-neoblue py-4 decoration-2 underline-offset-8" href="#section3">Portfolio</a></li>
-          </ul>
-      </nav>
-      
-    );
   }
 
 export default Navigation;
