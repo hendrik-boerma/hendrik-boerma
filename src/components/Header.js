a2d7410da814c659f17c73934152bf9ab55c6b86
Hendrik Boerma <hendrikboerma@gmail.com>
2023-09-29 14:52:01 +0200

    test

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ function Header({ tabIndex}) {
         <article className='w-full flex flex-col text-start justify-center gap-4 px-4 py-8 md:px-32 z-10 bg-backgroundcolor2'>
           <h1 className='text-textcolor text-3xl font-bold md:text-4xl xl:text-6xl'>Hendrik Boerma</h1>
           <h2 className='text-secondary text-xl md:text-2xl xl:text-3xl'>Front-end Developer</h2>
-          <p className='leading-relaxed text-l xl:text-xl text-textcolor py-4'>"Front-end developer/designer en tester met een oog voor de eindgebruiker."</p>
+          <p className='leading-relaxed text-l xl:text-xl text-textcolor py-4'>"Front-end developer en tester met een oog voor de eindgebruiker."</p>
          <div className="flex gap-12 py-4 slideleft">
             <a href="https://github.com/hendrik-boerma" rel="noreferrer" target="_blank" tabIndex={tabIndex} aria-label="Github"><FontAwesomeIcon className="cursor-pointer text-textcolor hover:text-secondary" icon={faGithub} size='2xl'/></a>
             <a href="https://www.linkedin.com/in/hendrik-boerma/" rel="noreferrer" target="_blank" tabIndex={tabIndex} aria-label="Linkedin"><FontAwesomeIcon className="cursor-pointer text-textcolor  hover:text-secondary" icon={faLinkedinIn} size='2xl'/></a>
