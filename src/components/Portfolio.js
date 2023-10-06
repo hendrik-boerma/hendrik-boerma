7a0b88d74130af6884e9db7f2b5a74d4fadacd86
Hendrik Boerma <hendrikboerma@gmail.com>
2023-10-06 14:29:50 +0200

    wip

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -77,43 +77,43 @@ function Portfolio({ tabIndex, setTabIndex }) {
                     ))}
                 </div>
                 <div className={toggleProject ? "flex items-center justify-center fixed w-full h-full bg-backgroundmodal top-0 right-0 z-20" : "hidden"}>
-                    <div className={"overflow-y-auto bg-backgroundcolor2 drop-shadow-2xl w-full lg:w-auto h-screen absolute lg:h-auto p-4 md:p-8 py-16 md:py-8 lg:rounded-lg slideright ease-in-out duration-300"}>
-                        <button ref={overlayRef} className='ml-auto flex items-center justify-center text-xl font-bold bg-primary text-textcolor rounded-full p-2 w-8 h-8 hover:bg-backgroundcolor2 hover:text-secondary ease-in-out duration-300' onClick={() => closeProject()}><span>X</span></button>
-                        <article className=' flex justify-center'>
-                            <div className='py-16 md:px-8 flex items-start justify-center flex-col gap-4'>
-                            <h5 className='text-3xl font-bold text-textcolor'>{project.name}</h5>
-                            <p className='col-span-2 text-md font-regular text-textcolor'>{project.subtitle}</p>
-                            <p className='col-start-1 col-span-2 row-span-1 flex flex-wrap gap-2'>
-                                {project.tags.map((tag, tagIndex) => (
-                                    <span className='bg-primary px-2 py-1 rounded text-textcolor' key={tagIndex}>
-                                        {tag}
-                                    </span>
-                                ))}
-                            </p>
-                            <img className={project.image == null ? 'hidden' : "max-w-xl w-full"} src={project.image} alt={project.alt}/>
-                            <p className='col-span-2 text-md font-regular text-textcolor py-8 max-w-xl'>
-                                {project.description}
-                            </p>
-                            <a type="button" href={project.link} rel="noreferrer" target="_blank" className={project.link == null ? 'hidden' : 'bg-secondary p-4 rounded cursor-pointer max-w-xs hover:bg-primary hover:text-secondary text-center ease-in-out duration-300'}>{project.linktext} <FontAwesomeIcon icon={faExternalLink} size='sm' /></a>
-                        
+                    <div className={"overflow-y-auto h-screen bg-backgroundcolor2 drop-shadow-2xl w-full lg:w-auto absolute lg:h-auto p-4 md:p-8 py-16 md:py-8 lg:rounded-lg slideright ease-in-out duration-300"}>
+                        <button ref={overlayRef} aria-label="Sluit" className='ml-auto sticky p-2 top-0 flex items-center justify-center text-xl font-bold bg-primary text-textcolor rounded-full w-8 h-8 hover:bg-backgroundcolor2 hover:text-secondary duration-300' onClick={() => closeProject()}><span>X</span></button>
+                        <article className='flex justify-center'>
+                            <div className='py-8 md:px-8 flex items-start justify-center flex-col gap-4'>
+                                <h5 className='text-3xl font-bold text-textcolor'>{project.name}</h5>
+                                <p className='col-span-2 text-md font-regular text-textcolor'>{project.subtitle}</p>
+                                <p className='col-start-1 col-span-2 row-span-1 flex flex-wrap gap-2'>
+                                    {project.tags.map((tag, tagIndex) => (
+                                        <span className='bg-primary px-2 py-1 rounded text-textcolor' key={tagIndex}>
+                                            {tag}
+                                        </span>
+                                    ))}
+                                </p>
+                                <img className={project.image == null ? 'hidden' : "max-w-xl w-full"} src={project.image} alt={project.alt} />
+                                <p className='col-span-2 text-md font-regular text-textcolor py-4 max-w-xl'>
+                                    {project.description}
+                                </p>
+                                <a type="button" href={project.link} rel="noreferrer" target="_blank" className={project.link == null ? 'hidden' : 'bg-secondary p-4 rounded cursor-pointer max-w-xs hover:bg-primary hover:text-secondary text-center ease-in-out duration-300'}>{project.linktext} <FontAwesomeIcon icon={faExternalLink} size='sm' /></a>
+
+                            <div className='flex items-start gap-4 justify-left py-4' >
+                                <button
+                                    id="previous"
+                                    className={`text-2xl font-bold ${number === 0 ? 'text-backgroundcolor2 bg-backgroundcolor cursor-default hover:none' : 'text-textcolor bg-primary hover:bg-backgroundcolor2 hover:text-secondary'} ease-in-out duration-300 flex items-center justify-center rounded w-8 h-8 p-1`}
+                                    onClick={handlePreviousClick}
+                                >
+                                    &lt;
+                                </button>
+                                <button
+                                    id="next"
+                                    className={`text-2xl font-bold ${number === totalProjects ? 'text-backgroundcolor2 bg-backgroundcolor cursor-default hover:none' : 'text-textcolor bg-primary hover:bg-backgroundcolor2 hover:text-secondary'} ease-in-out duration-300 flex items-center justify-center w-8 h-8 rounded p-1`}
+                                    onClick={handleNextClick}
+                                >
+                                    &gt;
+                                </button>
                             </div>
-                            </article>
-                        <div className='flex items-start gap-4 justify-center' >
-                            <button
-                                id="previous"
-                                className={`text-2xl font-bold ${number === 0 ? 'text-backgroundcolor2 bg-backgroundcolor cursor-default hover:none' : 'text-textcolor bg-primary hover:bg-backgroundcolor2 hover:text-secondary'} ease-in-out duration-300 flex items-center justify-center rounded w-8 h-8 p-1`}
-                                onClick={handlePreviousClick}
-                            >
-                                &lt;
-                            </button>
-                            <button
-                                id="next"
-                                className={`text-2xl font-bold ${number === totalProjects ? 'text-backgroundcolor2 bg-backgroundcolor cursor-default hover:none' : 'text-textcolor bg-primary hover:bg-backgroundcolor2 hover:text-secondary'} ease-in-out duration-300 flex items-center justify-center w-8 h-8 rounded p-1`}
-                                onClick={handleNextClick}
-                            >
-                                &gt;
-                            </button>
-                        </div>
+                            </div>
+                        </article>
                     </div>
                 </div>
             </section>
