873d58f6d80695e24a88178de3e7f4b9ad92885a
Hendrik Boerma <hendrikboerma@gmail.com>
2024-07-05 13:11:33 +0200

    deploy

--- backend/config/server.js
+++ backend/config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS', ['APP_KEYS']),
+    keys: env.array('APP_KEYS', ['default_secret_key']),
   },
   admin: {
     url: '/admin',
