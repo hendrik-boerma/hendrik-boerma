176f106e45bea282a30a7234745037d2929b930a
Hendrik Boerma <hendrikboerma@gmail.com>
2024-07-10 15:45:59 +0200

    versionapi

--- frontend/src/sanityClient.js
+++ frontend/src/sanityClient.js
@@ -3,7 +3,8 @@ import { createClient } from '@sanity/client'
 const client = createClient({
   projectId: '7d7n1ptu',
   dataset: 'production',
-  useCdn: false
+  useCdn: false,
+  apiVersion: '2022-03-07'
 })
 
 export default client
