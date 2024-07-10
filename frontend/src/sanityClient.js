050cc7821a501016c253f597f9315216e2d270ea
Hendrik Boerma <hendrikboerma@gmail.com>
2024-07-10 12:17:57 +0200

    Cdn

--- frontend/src/sanityClient.js
+++ frontend/src/sanityClient.js
@@ -2,7 +2,8 @@ import { createClient } from '@sanity/client'
 
 const client = createClient({
   projectId: '7d7n1ptu',
-  dataset: 'production'    
+  dataset: 'production',
+  useCdn: false
 })
 
 export default client
