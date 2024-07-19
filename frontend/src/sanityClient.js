88dcfe602165dd82af6225deb758ce0966d46240
Hendrik Boerma <hendrikboerma@gmail.com>
2024-07-19 14:34:03 +0200

    usedn

--- frontend/src/sanityClient.js
+++ frontend/src/sanityClient.js
@@ -5,6 +5,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 const client = createClient({
   projectId: '7d7n1ptu',
   dataset: isProduction ? 'production' : 'development',
+  useCdn: false,
   apiVersion: '2022-03-07'
 })
 
