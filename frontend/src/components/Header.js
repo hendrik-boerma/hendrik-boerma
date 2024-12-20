acd7dabb512041d6f5455b2d595aae0f6fc0ca53
Hendrik Boerma <hendrikboerma@gmail.com>
2024-12-20 12:50:30 +0100

    404

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -36,11 +36,11 @@ function Header() {
           ${toggleMenu ? 'hidden' : 'w-[2rem]'}`} />
         </button>
         <ul className={toggleMenu ? 'flex flex-col gap-4 text-lg text-textcolor md:flex-row' : 'flex-row gap-4 text-lg text-textcolor hidden md:flex'}>
-          <NavLink className={({ isActive }) => (isActive ? 'linkvisited' : 'hover:underline hover:underline-offset-8 hover:text-secondary hover:-translate-y-1 duration-500')} 
+          <NavLink className={({ isActive }) => (isActive ? 'linkvisited' : 'hover:underline hover:underline-offset-8 hover:decoration-2 hover:text-secondary')} 
           onClick={() => window.innerWidth === 750 ? setToggleMenu(true) : setToggleMenu(false)} to="/">Over mij</NavLink>
-          <NavLink className={({ isActive }) => (isActive ? 'linkvisited' : 'hover:underline hover:underline-offset-8 hover:text-secondary hover:-translate-y-1 duration-500')}
+          <NavLink className={({ isActive }) => (isActive ? 'linkvisited' : 'hover:underline hover:underline-offset-8 hover:decoration-2 hover:text-secondary')}
           onClick={() => window.innerWidth === 750 ? setToggleMenu(true) : setToggleMenu(false)} to="/Experience">Ervaring</NavLink>
-          <NavLink className={({ isActive }) => (isActive ? 'linkvisited' : 'hover:underline hover:underline-offset-8 hover:text-secondary hover:-translate-y-1 duration-500')}
+          <NavLink className={({ isActive }) => (isActive ? 'linkvisited' : 'hover:underline hover:underline-offset-8 hover:decoration-2 hover:text-secondary')}
           onClick={() => window.innerWidth === 750 ? setToggleMenu(true) : setToggleMenu(false)} to="/Work">Werk</NavLink>
         </ul>
       </div>
--- frontend/src/pages/Work.js
+++ frontend/src/pages/Work.js
@@ -10,9 +10,6 @@ function Portfolio() {
         <>
             <section className='bg-darkblue flex flex-col gap-8' id='section3'>
                 <h1 className='text-3xl text-textcolor font-bold'>Werk</h1>
-                <p className='col-span-2 text-md leading-relaxed font-regular text-textcolor md:w-4/5'>
-                    Een aantal webapplicaties die ik gebouwd heb met onder andere React en Tailwind.
-                </p>
                 <div className="grid grid-cols-1 md:grid-cols-2 md:grid-cols-2 gap-8">
                     <Card contents={projects} />
                 </div>
